8142ec2ab4a0aaec93a8ab54dbfc582cb35f1d1a
Ibtehal-Thabet <55961675+Ibtehal-Thabet@users.noreply.github.com>
2020-03-24 21:37:53 +0200

    Add files via upload

---
+++ screens/loading.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import * as firebase from 'firebase';
+
+export default class loading extends React.Component {
+    render() {
+        return (
+            firebase.auth().onAuthStateChanged(user => {
+                this.props.navigation.navigate(user ? 'User' : 'Login')
+            })
+        )
+    }
+}
